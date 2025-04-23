40c742782ca415bbe948cabc9d0a3953cb30168e
github-ci <you@example.com>
2025-04-23 09:07:36 +0000

    refactor(wechat-official-account-market-code-bundle): 移除 AdminMenu 和简化包配置

--- src/WechatOfficialAccountMarketCodeBundle.php
+++ src/WechatOfficialAccountMarketCodeBundle.php
@@ -3,16 +3,7 @@
 namespace WechatOfficialAccountMarketCodeBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
-use Tourze\BundleDependency\BundleDependencyInterface;
-use Tourze\EasyAdmin\Attribute\Permission\AsPermission;
 
-#[AsPermission(title: '一物一码')]
-class WechatOfficialAccountMarketCodeBundle extends Bundle implements BundleDependencyInterface
+class WechatOfficialAccountMarketCodeBundle extends Bundle
 {
-    public static function getBundleDependencies(): array
-    {
-        return [
-            \WechatOfficialAccountBundle\WechatOfficialAccountBundle::class => ['all' => true],
-        ];
-    }
 }
